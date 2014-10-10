2accc25c7b82c30a96a9619b282043250d9d3ecf
e2thex <erik@e2thex.org>
2014-10-09 22:08:05 -0500

    clean up engine and added caching for scoring

--- app/js/engine.js
+++ app/js/engine.js
@@ -6,98 +6,22 @@ var engine = function(items, events, scoringEngine, eventEngine) {
   that.eventEngine = typeof eventEngine =='undefined' ? eventEngine() : eventEngine ;
   that.eventEngine = eventEngine,
   that.currentRank =[];
+  that.results = false;
   that.recordEventResult = function recordEvent(eEvent, result) {
     if (result != null) {
       eEvent.recordResult(result, Date.now());
       that.events.push(eEvent);
+      that.results = false;
     }
   }
   that.createEvent = function() {
     return this.eventEngine.createEvent(this.score(), this.events);
   }
   that.score = function() {
-    return this.scoringEngine.score(this.items, this.events);
-  }
-  that.getNewEvent = function() {
-    // shuffle all items
-    var items = shuffle(this.getRankedItems());
-    // pick a random first itle
-    var itemA = items.pop();
-    //complile helpfull data
-    //max diff max Events and all candidates
-    var data = items.reduce(function(data, item) {
-      data.maxEvents = item.events>data.maxEvents ? item.events: data.maxEvents;
-      data.minEvents = item.events<data.minEvents ? item.events: data.minEvents;
-      var diff = Math.abs(itemA.score - item.score);
-      data.maxDiff = diff>data.maxDiff ? diff: data.maxDiff;
-      data.minDiff = diff<data.minDiff ? diff: data.minDiff;
-      //add a combo of this item with each of the previous items as a candidate
-      var newDCs =data.previous.map(function(pitem) {
-        return [item, pitem];
-      });
-      data.candidates = data.candidates.concat(newDCs)
-      //add this item as a candidate
-      data.candidates.push([item]);
-      
-      data.previous.push(item);
-      return data;
-    }, {maxEvents:0, minEvents:10000, maxDiff:0, minDiff:15000, previous:[], candidates: []});
-    //create a next score for each candidate
-    var candidates = data.candidates.map(function(items) {
-      var cData = items.reduce(function(cData, item) {
-        cData.score += item.score;
-        cData.events = item.events>cData.events ? item.events : cData.events;
-        cData.items.push(item.item);
-        return cData;
-      }, {score:0, events:0, items:[]});
-      var nextScore = 
-        10*(cData.events/data.maxEvents) +
-        10*(Math.abs(itemA.score-cData.score)/data.maxDiff) +
-        Math.random();
-        
-      return {
-        nextScore:nextScore,
-        items:cData.items
-      };
-    });
-    candidates.sort(function(a,b) {return a.nextScore-b.nextScore});
-    return engineEvent([itemA.item], candidates[0].items);
-  }
-  that.getRankedItems = function() {
-    var start = 1000;
-    var factor = .1;
-    var initScores = this.items.reduce(function(scores, item) {
-      scores[item.key] = {score: start, item:item, events:0};
-      return scores;
-    }, {});
-    var scores = this.events.reduce(function(scores, eEvent) {
-      var winnersScore = eEvent.winners.reduce(function(score, item) {
-        return score + scores[item.key].score;
-      },0);
-      var losersScore = eEvent.losers.reduce(function(score, item) {
-        return score + scores[item.key].score;
-      },0);
-      var scoreDiff = winnersScore - losersScore;
-      var scoreChange = (Math.max(winnersScore, losersScore)-scoreDiff) *factor
-      var maxScore = start;
-      eEvent.winners.map(function(item) {
-        scores[item.key].score = Math.floor(scores[item.key].score + scoreChange/eEvent.winners.length);
-        maxScore = scores[item.key].score >maxScore ? scores[item.key].score : maxScore;
-        scores[item.key].events ++;
-      });
-      eEvent.losers.map(function(item) {
-        scores[item.key].score = Math.floor(scores[item.key].score - scoreChange/eEvent.losers.length);
-        scores[item.key].events ++;
-      });
-      for (key in scores) {
-        scores[key].score = start * (scores[key].score/maxScore);
-      }
-      return scores;
-    }, initScores);
-    var scoreTable = Object.keys(scores).map(function(key) {
-      return scores[key];
-    }).sort(function(a,b) { return b.score-a.score});
-    return scoreTable;
+    if(!this.results) {
+      this.results = this.scoringEngine.score(this.items, this.events);
+    }
+    return this.results;
   }
   return that;
 }
